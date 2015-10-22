fe8c6f00ec772357ad8a9e1eeb56b36c9f665ec7
Lucas <lucas.a.aranda@gmail.com>
2015-10-22 19:19:41 -0300

    Organizacion del proyecto

--- src/server/XmlParcerExpert.java
+++ src/server/XmlParcerExpert.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package main;
+package server;
 
+import model.Mail;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
